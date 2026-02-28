3e71c6a8fdd45ee0e3c064a4d24c9e2e22bea075
Trần Xuân Trường <techoigame437@gmail.com>
2026-02-28 18:12:56 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import react from "@vitejs/plugin-react";
 export default {
   plugins: [react()],
   root: "src/renderer",
+  base: "./", // Quan trọng: dùng đường dẫn tương đối để load đúng khi chạy từ file:// (app đóng gói)
   build: {
     outDir: "../../dist/renderer",
-    emptyOutDir: true
-  }
+    emptyOutDir: true,
+  },
 };
